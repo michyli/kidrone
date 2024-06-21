9f77294de28381fc209a3f143902b049a6b7d5e7
Michael Li <michaelli.fr@gmail.com>
2024-06-21 13:35:45 -0400

    fix bugs

--- run.py
+++ run.py
@@ -45,8 +45,8 @@ path = generate_path(eg2, 2, 0.1, vis=True) """
 """ eg3 = [(20, 10), (36, 19), (50, 15), (55, 22), (60, 38), (40, 40), (30, 50), (20, 43), (27, 30), (21, 20)] #More complicated shape
 path = generate_path(eg3, 3, 9, vis=True) """
 
-""" eg4 = [(20, 10), (36, 19), (50, 15), (55, 22), (60, 38), (40, 40), (30, 50), (20, 43), (27, 30), (21, 20)] #More complicated shape
-path = generate_path(eg4, 0.8, 0.5, vis=True) """
+eg4 = [(20, 10), (36, 19), (50, 15), (55, 22), (60, 38), (40, 40), (30, 50), (20, 43), (27, 30), (21, 20)] #More complicated shape
+path = generate_path(eg4, 0.8, 0.5, vis=True)
 
 #Demonstrates what the returned data type looks like (list of LineString object)
 """print(np.array(path))"""
