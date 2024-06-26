fix: angle formula, stage: for implementation of multiple coordinate systems using pyplot

basic_functions.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from shapely.geometry import LineString, Point, LinearRing, MultiPoint, Polygon
-from math import acos, sqrt, pi
 
 """
 ======================
@@ -11,6 +10,8 @@ from math import acos, sqrt, pi
 Here is a list of general functions used in the rest of the algorithm.
 They are very helpful to manipulate Shapely datatypes and vectors to gear towards a general purpose.
 """
+
+
 
 def normalizeVec(x, y):
     """Normalize a vector (x, y)"""
@@ -73,21 +74,20 @@ def line_angle(line1: LineString, line2: LineString):
     if line1.coords[-1] != line2.coords[0] and line2.coords[-1] != line1.coords[0]:
         raise ValueError("The lines are not continuous")
     
+    if line2.coords[-1] == line1.coords[0]:
+        #if line sequence is 2 -> 1, swap them
+        temp = line1
+        line1 = line2
+        line2 = temp
+    
     #vectorize two LineStrings
     vec1 = (line1.coords[1][0] - line1.coords[0][0], line1.coords[1][1] - line1.coords[0][1])
     vec2 = (line2.coords[1][0] - line2.coords[0][0], line2.coords[1][1] - line2.coords[0][1])
+    vec1n = normalizeVec(vec1[0], vec1[1])
+    vec2n = normalizeVec(vec2[0], vec2[1])
 
-    dot_product = (vec1[0] * vec2[0]) + (vec1[1] * vec2[1])
-    magnitude1 = sqrt(vec1[0] ** 2 + vec1[1] ** 2)
-    magnitude2 = sqrt(vec2[0] ** 2 + vec2[1] ** 2)
-
-    angle_rad = acos(dot_product / (magnitude1 * magnitude2))
-    angle_deg = angle_rad * (180 / pi)
-
-    if angle_deg > 180:
-        angle_deg = 360 - angle_deg
-    angle_deg = 180 - angle_deg
-    return angle_deg
+    angle = np.rad2deg(np.arccos(np.dot(vec1n, vec2n)))
+    return angle
 
 def pt_to_line(point: Point, line: LineString):
     """Orthogonally projects a point onto a line

polygoncreate.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from pyproj import Geod
 from shapely.geometry import LineString, Point, LinearRing, MultiPoint, Polygon
 from basic_functions import *
 from path import *
@@ -13,7 +14,7 @@ class PolygonCreate:
     """
     Creates a polygon object with inherit attributes.
     """
-    def __init__(self, points, children=None):
+    def __init__(self, points, children=None, offsetparent=None):
         """ Initialization of Polygon object.
         
         points:     tuple / list of (x, y) coordinates
@@ -42,6 +43,7 @@ class PolygonCreate:
                 if not c.within(self.polygon):
                     raise AssertionError("The children are not fully contained in the overall polygon.")
         self.children = children
+        self.offsetparent = offsetparent
 
     def poly_offset(self, offset):
         """ Returns a new PolygonCreate object of the offsetted polygon, where
@@ -90,7 +92,7 @@ class PolygonCreate:
             newY.append(oldY[curr] + bislen * bisnY)
 
             new_points = [(newX[i], newY[i]) for i in range(len(newX))]
-        return PolygonCreate(new_points)
+        return PolygonCreate(new_points, self.children, self)
 
     def extrapolate_line(self, point: Point, slope):
         """ Construct a line from a point and a slope, then extend a line to the maximum boundry of the polygon.
@@ -256,10 +258,11 @@ class PolygonCreate:
         for line in complete_path:
             final_path.extend(break_line(line))
         
+        print(np.array(final_path))
         return Path(final_path, self, opp_slope)
 
     def showpoly(self, polys = None):
-        """ Plots the polygon, with the option to plot additional polygon on the same plot
+        """Plots the polygon, with the option to plot additional polygon on the same plot
         
         poly: a list of CreatePolygon objects that will be plotted in addition to 'self'
 