724ee81f2eab32469a652c1e5b6909c6342a93d0
Michael Li <michaelli.fr@gmail.com>
2024-07-04 16:26:55 -0400

    receiving pull

--- run.py
+++ run.py
@@ -12,14 +12,24 @@ from src.optimization import *
             [-119.3069674, 47.6949743]] """
   
 
-points = csv2coords("test_coordinates.csv")                 #extract coords from test_coordinates.csv file
-disp_diam = 10                                             #meters
+""" points = csv2coords("test_coordinates.csv")                 #extract coords from test_coordinates.csv file
+disp_diam = 10                                              #meters
 
 pathlist, runtime = path_list_constructor(points, disp_diam)#construct a list of path to iterate over
-print(f"runtime is {runtime}")
+print(f"runtime is {runtime}") 
 optimized_path = optimizer(pathlist, shortest_airtime())    #calculates the optimized path
 showpath(optimized_path)                                    #show the optimized path
+ """
 
 
+p1 = (0, 0)
+p2 = (0, 1000)
+p3 = (1000, 1000)
+p4 = (1000, 0)
+p5 = (0, 0)
+points = [p1, p2, p3, p4, p5]
+new_points = pcs2gcs_batch(points)
+outline = Outline('test', new_points)
+path = outline.swath_gen(10, 0)
 
 
