1b6f251a8c95cd3e849301a7a182ab986e21eaf3
Wen Qi <wendy.qi@berkeley.edu>
2024-07-09 15:12:51 -0400

    clean up code

--- static/mapInteractions.js
+++ static/mapInteractions.js
@@ -1,62 +1,115 @@
+var map, view, graphicsLayer;
+
 require([
     "esri/Map",
     "esri/views/MapView",
     "esri/Graphic",
-    "esri/layers/GraphicsLayer"
+    "esri/layers/GraphicsLayer",
 ], function (Map, MapView, Graphic, GraphicsLayer) {
-    var map = new Map({
-        basemap: "streets"  // Choose the basemap suitable for your needs
+    map = new Map({
+        basemap: "streets",
     });
-
-    var view = new MapView({
+    view = new MapView({
         container: "viewDiv",
         map: map,
-        center: [-79.4637, 43.6465], // Longitude, Latitude for High Park, Toronto
-        zoom: 15  // Increased zoom level for a closer look
+        center: [-79.4637, 43.6465],
+        zoom: 15,
     });
-
-    var graphicsLayer = new GraphicsLayer();
+    graphicsLayer = new GraphicsLayer();
     map.add(graphicsLayer);
-
-    view.on("click", function(event) {
+    view.on("click", function (event) {
         var lat = event.mapPoint.latitude;
         var lon = event.mapPoint.longitude;
-
         var point = {
             type: "point",
             longitude: lon,
-            latitude: lat
+            latitude: lat,
         };
-
         var simpleMarkerSymbol = {
             type: "simple-marker",
-            color: [226, 119, 40], // Orange
+            color: [226, 119, 40],
             outline: {
-                color: [255, 255, 255], // White
-                width: 1
-            }
+                color: [255, 255, 255],
+                width: 1,
+            },
         };
-
         var pointGraphic = new Graphic({
             geometry: point,
-            symbol: simpleMarkerSymbol
+            symbol: simpleMarkerSymbol,
         });
-
         graphicsLayer.add(pointGraphic);
-
-        fetch('/store-coords', {
-            method: 'POST',
+        fetch("/store-coords", {
+            method: "POST",
             headers: {
-                'Content-Type': 'application/json',
+                "Content-Type": "application/json",
             },
-            body: JSON.stringify({ latitude: lat, longitude: lon })
+            body: JSON.stringify({ latitude: lat, longitude: lon }),
         })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Success:', data);
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("Success:", data);
         })
         .catch((error) => {
-            console.error('Error:', error);
+            console.error("Error:", error);
         });
     });
 });
+
+function submitForm() {
+    const form = document.getElementById("optimize-form");
+    const formData = new FormData(form);
+    fetch("/optimize", {
+        method: "POST",
+        body: formData,
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        if (data.error) {
+            document.getElementById("output").innerText = data.error;
+        } else {
+            document.getElementById("output").innerText = data.result;
+            const plotDiv = document.getElementById("plot");
+            plotDiv.innerHTML = "";
+            const plot = document.createElement("img");
+            plot.src = `/static/${data.plot_path}`;
+            plotDiv.appendChild(plot);
+            drawPathOnMap(data.best_path);
+        }
+        const debugDiv = document.getElementById("debug");
+        debugDiv.innerHTML = "";
+        data.debug.forEach((line) => {
+            const p = document.createElement("p");
+            p.innerText = line;
+            debugDiv.appendChild(p);
+        });
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+    });
+}
+
+function drawPathOnMap(pathDetails) {
+    console.log("Drawing path with details:", pathDetails);
+    require(["esri/Graphic"], function (Graphic) {
+        pathDetails.forEach((segment) => {
+            const polyline = {
+                type: "polyline",
+                paths: [
+                    [segment.start.longitude, segment.start.latitude],
+                    [segment.end.longitude, segment.end.latitude],
+                ],
+            };
+            console.log("Polyline details:", polyline);
+            const simpleLineSymbol = {
+                type: "simple-line",
+                color: "#8A2BE2",
+                width: "2",
+            };
+            const polylineGraphic = new Graphic({
+                geometry: polyline,
+                symbol: simpleLineSymbol,
+            });
+            graphicsLayer.add(polylineGraphic);
+        });
+    });
+}
