d1ccd29b7009b6d91e7ee1a5d03bfd4f5b521b45
Michael Li <michaelli.fr@gmail.com>
2024-07-11 23:34:28 -0400

    check merge

---
+++ basic_functions.py
@@ -0,0 +1,285 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from pyproj import Transformer
+from shapely.geometry import LineString, Point, MultiPoint, Polygon
+import random
+import csv
+
+"""
+======================
+=== Basic Functions===
+======================
+
+Here is a list of general functions used in the rest of the algorithm.
+They are very helpful to manipulate Shapely datatypes and vectors to gear towards a general purpose.
+"""
+
+"""
+======================
+=== Vector Related ===
+======================
+"""
+def normalizeVec(x, y):
+    """Normalize a vector (x, y)"""
+    
+    if x == 0:
+        assert y != 0, "Zero vector cannot be normalized"
+    if y == 0:
+        assert x != 0, "Zero vector cannot be normalized"
+    
+    norm = 1 / np.sqrt(x ** 2 + y ** 2)
+    return x * norm, y * norm
+
+
+"""
+==========================
+=== LineString Related ===
+==========================
+"""
+def line_slope(line: LineString):
+    """Returns the slope of a LineString based on the boundary of the LineString"""
+    assert isinstance(line, LineString), "input should be a LineString object"
+    if (line.boundary.geoms[1].x - line.boundary.geoms[0].x) != 0:
+        slope = (line.boundary.geoms[1].y - line.boundary.geoms[0].y) / (line.boundary.geoms[1].x - line.boundary.geoms[0].x)
+    else:
+        slope = "vertical"
+    return slope
+
+def line_angle(line1: LineString, line2: LineString):
+    """Returns the smaller angle formed by two line segments.
+    Note that the direction of the LineString objects matters.
+    Two line segments should be consecutive, meaning that they should intersect at at least one point.
+    Output angle is in degrees.
+    """
+    assert all(isinstance(i, LineString) for i in [line1, line2]), "input lines should be Line objects"
+    
+    #Check for continuity
+    if line1.coords[-1] != line2.coords[0] and line2.coords[-1] != line1.coords[0]:
+        raise ValueError("The lines are not continuous")
+    
+    if line2.coords[-1] == line1.coords[0]:
+        #if line sequence is 2 -> 1, swap them
+        temp = line1
+        line1 = line2
+        line2 = temp
+    
+    #vectorize two LineStrings
+    vec1 = (line1.coords[1][0] - line1.coords[0][0], line1.coords[1][1] - line1.coords[0][1])
+    vec2 = (line2.coords[1][0] - line2.coords[0][0], line2.coords[1][1] - line2.coords[0][1])
+    vec1n = normalizeVec(vec1[0], vec1[1])
+    vec2n = normalizeVec(vec2[0], vec2[1])
+
+    angle = np.rad2deg(np.arccos(np.dot(vec1n, vec2n)))
+    #Range of arccos() is (0, pi) or (0, 180)
+    return angle  
+
+def line_intersection(point1: Point, slope1, point2: Point, slope2):
+    """Finds the point of intersection between two straight lines given the point and slope of both lines.
+    
+    slope1 and slope2 can be either given as number or "vertical"
+    """
+    
+    assert all(isinstance(i, Point) for i in [point1, point2]), "input point should be Point objects"
+    assert isinstance(slope1, (int, float)) or slope1 == "vertical", "input slope1 should be a number or 'vertical'"
+    assert isinstance(slope2, (int, float)) or slope2 == "vertical", "input slope2 should be a number or 'vertical'"
+    
+    if slope1 == "vertical" and slope2 == "vertical":
+        if point1 == point2:
+            raise ValueError("two inputted lines are the same")
+        else:
+            raise ValueError("No intersections, two inputted lines are parallel")
+    if slope1 == "vertical":
+        return Point([point1.x, point2.y + slope2 * (point1.x - point2.x)])
+    if slope2 == "vertical":
+        return Point([point2.x, point1.y + slope1 * (point2.x - point2.x)])
+        
+    b1 = point1.y - slope1 * point1.x
+    b2 = point2.y - slope2 * point2.x
+    
+    x = (b2 - b1) / (slope1 - slope2)
+    y = slope1 * x + b1
+    return Point([x, y])
+
+def split_line(line: LineString, interval) -> list[Point]:
+    """Splits a line into equal distances
+        if a line can't be split into the given interval distance, and there are less than 20% of interval as excess,
+            then it expands the interval slightly
+        if the excess is more than 20% of the interval,
+            then it shrinks the interval to add in another split
+   
+    line:       a LineString object to be split
+    interval:   the desired distance between points
+    
+    return a list of Point objects
+    """
+    assert isinstance(line, LineString), "input line should be a LineString object"
+    
+    num_div = line.length / interval
+    if num_div % 1 < 0.2 and num_div % 1 > 0:
+        pass
+    else:
+        num_div += 1
+    
+    distances = np.linspace(0, line.length, int(num_div))
+    points = [line.interpolate(distance) for distance in distances] + [line.boundary.geoms[-1]]
+    if points[-1] == points[-2]:
+        points.pop(-1)
+    return points
+
+def reverse_line(line: LineString):
+    """Reverses the order of points in the LineString
+    line: a LineString object with two or more points to be reversed in direction
+    """
+    assert isinstance(line, LineString), "input line should be a LineString object"
+    line_list = np.array(line.coords)
+    return LineString(line_list[::-1])
+
+def break_line(line: LineString) -> list[LineString]:
+    """Break a multi-point LineString into a list of multiple 2-point LineStrings
+    Returns a list of LineString objects
+    
+    line: a LineString object to be broken down
+    """
+    assert isinstance(line, LineString), "input line should be a LineString object"
+    coords = list(line.coords)
+    return [LineString([coords[i], coords[i+1]]) for i in range(len(coords)-1)]
+
+def merge_line(linelist: list[LineString]):
+    """Merge a list of continuous 2-point LineStrings into one single multi-point LineString
+    Returns a single LineString object
+    """
+    coords = []
+    start_coord = None
+    for line in linelist:
+        firstpoint = line.boundary.geoms[0]
+        secondpoint = line.boundary.geoms[1]
+        if not start_coord:
+            coords.append(firstpoint)
+            coords.append(secondpoint)
+            start_coord = secondpoint
+        else:
+            assert firstpoint == start_coord, "Lines are not continuous"
+            coords.append(secondpoint)
+            start_coord = secondpoint
+    return LineString(coords)            
+
+def check_continuity(lines: list[LineString]):
+    """check if the list of lines are continuous
+    
+    lines: a list of LineStrings to be checked
+    """
+    count = 1
+    for i in range(1, len(lines)-1):
+        assert lines[i-1].boundary.geoms[1] == lines[i].boundary.geoms[0], f"line{i-1} and line{i} are not continuous"
+        count += 1
+    return True
+
+
+"""
+================================
+=== Coordinates Manipulation ===
+================================
+"""
+def pt_to_line(point: Point, line: LineString):
+    """Orthogonally projects a point onto a line
+    returns the projected point as a Point object
+    
+    point:  the point being projected
+    line:   the line being projected onto
+    """
+    assert isinstance(point, Point) and isinstance(line, LineString), "input should be a Point object and a LineString object"
+    
+    x = np.array(point.coords[0])
+
+    u = np.array(line.coords[0])
+    v = np.array(line.coords[len(line.coords)-1])
+
+    n = v - u
+    n /= np.linalg.norm(n, 2)
+
+    P = u + n*np.dot(x - u, n) #datatype if np.array
+    return Point([P[0], P[1]])
+
+def extract_coords(shape: Point|MultiPoint|LineString) -> list[tuple]:
+    """Extracts Shapely geometry (Point, MultiPoint, LineString) coordinates into a list of tuples.
+    Returns the extracted list of coordinates
+    
+    shape: a Point, MultiPoint, or LineString object to be flattened
+    """
+    assert isinstance(shape, (Point, MultiPoint, LineString)), "input must be a Point, MultiPoint, or LineString object"
+    
+    if isinstance(shape, Point):
+        return [(shape.x, shape.y)]
+    if isinstance(shape, MultiPoint):
+        return [(pt.x, pt.y) for pt in shape.geoms]
+    if isinstance(shape, LineString):
+        return [(pt[0], pt[1]) for pt in shape.coords]
+
+def csv2coords(csvfile):
+    """extract coordinate from a .csv file"""
+    with open(csvfile, 'r') as csvfile:
+        next(csvfile)
+        csv_reader = csv.reader(csvfile)
+        coords = [(row[0], row[1]) for row in csv_reader]
+    return coords
+
+
+"""
+==================================
+=== Coordinates Transformation ===
+==================================
+"""
+def gcs2pcs(lon, lat):
+    """Converts EPSG:4326 (lon&lat) to EPSG:3857 (meters)
+    """
+    transformer = Transformer.from_crs("EPSG:4326", "EPSG:3857", always_xy=True)
+    x, y = transformer.transform(lon,lat)
+    if x == np.inf or y == np.inf:
+        raise ValueError("input should be in sequence of (Longtitude, Latitude), it may be currently reversed")
+    return x, y
+
+def pcs2gcs(x, y):
+    """Converts EPSG:3857 (meters) to EPSG:4326 (lat&lon)
+    """
+    transformer = Transformer.from_crs("EPSG:3857", "EPSG:4326", always_xy=True)
+    lon, lat = transformer.transform(x,y)
+    return lon, lat
+
+def gcs2pcs_batch(coords):
+    """Converts EPSG:4326 (lon&lat) to EPSG:3857 (meters)
+    but input is a whole list of EPSG:4326 coordinates
+    """
+    return [gcs2pcs(pt[0], pt[1]) for pt in coords]
+    
+def pcs2gcs_batch(coords):
+    """Converts EPSG:3857 (meters) to EPSG:4326 (lat&lon)
+    but input is a whole list of EPSG:3857 coordinates
+    """
+    return [pcs2gcs(pt[0], pt[1]) for pt in coords]
+
+
+
+"""
+==============
+=== Others ===
+==============
+"""
+def arbit_list(num, min, max):
+    """
+    Function used for generating test data for show3DPath
+
+    Generate a list of tuples representing heights for each line segment.
+    
+    num_lines: The number of LineString objects.
+    min_height: The minimum height value.
+    max_height: The maximum height value.
+    """
+    heights = np.linspace(min, max, num)
+    values = [(heights[i], heights[i+1]) for i in range(len(heights)-1)]
+    return values
+
+def disp_time(hour):
+    """Convert inputed hours to the format of day:hour:minute"""
+    return f"This path is projected to take {int(hour//24)} days, {int(hour%24//1)} hours, and {round(hour%1*60, 1)} minutes"
+
+
---
+++ graph.py
@@ -0,0 +1,100 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from basic_functions import *
+
+"""
+An aggregation of all graphing functions
+"""
+def showpoly(ax, polygons, label=None, color=None):
+        """Plots polygon
+        ax:         The axes to plot on
+        polygon:    an Outline objects to be graphed
+        """
+        if polygons.children:
+            for child in polygons.children:
+                showpoly(ax, child, color="darkcyan")
+     
+        xx, yy = polygons.polygon.exterior.xy
+        ax.plot(xx, yy, '-.', color=color, label=label, ms=4)
+        return ax
+
+def showpath(path):
+    """plot all relevant information about the path
+    path: a Path instance
+    """
+    fig1 = plt.figure(figsize=(16, 8))
+    fig1.suptitle("Full Coverage Drone Flight Path", fontsize=16)
+    ax1 = fig1.add_subplot(1, 3, 1)
+    ax2 = fig1.add_subplot(1, 3, 2, projection='3d')
+    ax3 = fig1.add_subplot(1, 3, 3)
+    
+    path.path_disp(ax1)       #plot path
+    path.airtime_print()      #print airtime
+    path.coverage_print()     #print coverage
+    path.length_print()       #print path length
+    
+    heights = arbit_list(len(path.path), 10, 30)
+    velocity = [seg.curr_velo for seg in path.segment_list]
+    show3DPath(ax2, path, ("height", heights))
+    path.coverage_disp(ax3)   #plot coverage
+    
+    plt.tight_layout()
+    plt.show()
+
+def showprojection(ax, full_path):
+    """
+    Plots the baseline and field projection on the XY plane.
+    
+    ax: The 3D axis object to plot on.
+    full_path: a Path object. The .path attribute extracts the list of LineString that makes the Path object.
+    """
+    #plot projected path
+    for lines in full_path.path:
+        start, end = lines.coords[0], lines.coords[1]
+        xx, yy = [start[0], end[0]], [start[1], end[1]]
+        zz = [0, 0]  # Baseline at z=0
+        ax.plot(xx, yy, zz, 'k--', ms=4, linewidth=1)
+    
+    #plot field outline
+    if full_path.parent.offsetparent:
+        x1, y1 = full_path.parent.offsetparent.polygon.exterior.xy
+        ax.plot(x1, y1, 0, color="teal", linestyle="-.")
+        x2, y2 = full_path.parent.polygon.exterior.xy
+        ax.plot(x2, y2, 0, color="paleturquoise", linestyle=":")
+    else:
+        x1, y1 = full_path.parent.polygon.exterior.xy
+        ax.plot(x1, y1, 0, color="teal", linestyle=":")
+
+def show3DPath(ax, full_path, z_values):
+    """
+    Plots the complete 3D path.
+    
+    full_path: a Path object. The .path attribute extracts the list of LineString that makes the Path object.
+    values: an iterable (e.g., list, numpy array) with the same length as the number of lines in Path.
+            Can represent velocity, height, etc.
+    """
+    #Determine what the z-parameter is
+    name, z_val = z_values[0], z_values[1]
+    
+    # Plot each LineString with its start and end points at the correct height
+    for lines, (start_value, end_value) in zip(full_path.path, z_val):
+        start, end = lines.coords[0], lines.coords[1]
+        xx, yy = [start[0], end[0]], [start[1], end[1]]
+        zz = [start_value, end_value]
+        ax.plot(xx, yy, zz, color='g', linestyle='-', marker=None, linewidth=2.5)
+        
+        # Plot vertical lines to connect path and baseline
+        ax.plot([start[0], start[0]], [start[1], start[1]], [0, start_value], 'r-.', linewidth=1, alpha=0.5)
+        ax.plot([end[0], end[0]], [end[1], end[1]], [0, end_value], 'r-.', linewidth=1, alpha=0.5)
+
+    showprojection(ax, full_path)
+
+    ax.set_title(f"3D View with {name.capitalize()}")
+    ax.set_xlabel('x (m)')
+    ax.set_ylabel('y (m)')
+    ax.xaxis.set_major_locator(ticker.NullLocator())
+    ax.yaxis.set_major_locator(ticker.NullLocator())
+    ax.zaxis.set_major_locator(plt.MaxNLocator(4))
+    ax.set_zlabel(f'{name} ({"KM/h" if name=="velocity" else "m"})')
+    ax.set_box_aspect((2,2,1.5))
