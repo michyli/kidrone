a5fc07dfda94cdc80ad480e7e60c99ef2da419de
Michael Li <michaelli.fr@gmail.com>
2024-07-29 13:51:46 -0400

    complete tab transition

--- electron/gui/assets/js/landing.js
+++ electron/gui/assets/js/landing.js
@@ -1,6 +1,19 @@
 const burger = document.querySelector(".burgerMenu");
 const menu = document.querySelector(".menu");
+const titles = document.querySelectorAll(".menu div");
 
+const newProject = document.getElementById("new");
+const browseProject = document.getElementById("browse");
+const loadProject = document.getElementById("load");
+const settingProject = document.getElementById("setting");
+const newProjectTab = document.getElementById("newProject");
+const browseProjectTab = document.getElementById("browseProject");
+const loadProjectTab = document.getElementById("loadProject");
+const settingProjectTab = document.getElementById("settingProject");
+
+menu.style.height = `${menu.scrollHeight + 20}px`;
+
+/* Burger & Menu Control */
 burger.addEventListener("click", function() {
   burger.classList.toggle("active")
   if (burger.classList.contains("active")) {
@@ -8,4 +21,37 @@ burger.addEventListener("click", function() {
   } else {
     menu.style.height = '0px';
   }
+})
+
+titles.forEach((title) => {
+  title.addEventListener("click", function() {
+    burger.classList.remove("active")
+    menu.style.height = '0px';
+  })
+})
+
+/* Switch Tabs logic */
+newProject.addEventListener("click", function() {
+  newProjectTab.classList.add("active");
+  browseProjectTab.classList.remove("active");
+  loadProjectTab.classList.remove("active");
+  settingProjectTab.classList.remove("active");
+})
+browseProject.addEventListener("click", function() {
+  newProjectTab.classList.remove("active");
+  browseProjectTab.classList.add("active");
+  loadProjectTab.classList.remove("active");
+  settingProjectTab.classList.remove("active");
+})
+loadProject.addEventListener("click", function() {
+  newProjectTab.classList.remove("active");
+  browseProjectTab.classList.remove("active");
+  loadProjectTab.classList.add("active");
+  settingProjectTab.classList.remove("active");
+})
+settingProject.addEventListener("click", function() {
+  newProjectTab.classList.remove("active");
+  browseProjectTab.classList.remove("active");
+  loadProjectTab.classList.remove("active");
+  settingProjectTab.classList.add("active");
 })
