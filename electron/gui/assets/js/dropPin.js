782046416c3d99b5547e66c815ab025dc87a758b
Edward Cheng <edwche577@berkeley.edu>
2024-07-30 15:30:17 -0400

    drop pin bug fixed

--- electron/gui/assets/js/dropPin.js
+++ electron/gui/assets/js/dropPin.js
@@ -2,5 +2,6 @@ import { switchSubTab } from "./landing.js";
 
 const dropPinButton = document.getElementById("dropPinButton");
 dropPinButton.addEventListener("click", () => {
+    console.log("click")
     switchSubTab("dropPinF1");
 });
