57473f8ee113730efd9eaaa75a7f3cb552a0afd9
Edward Cheng <edwche577@berkeley.edu>
2024-08-01 12:07:09 -0400

    Working save settings, even if program closes

--- electron/gui/assets/js/generateFlight.js
+++ electron/gui/assets/js/generateFlight.js
@@ -1,11 +1,26 @@
 import { switchSubTab } from "./landing.js";
+import { displaySettingsValues } from "./setting.js";
 
 const dropPinButton = document.getElementById("planFlightBtn");
 dropPinButton.addEventListener("click", () => {
     switchSubTab("generateFlightF1");
+
+    //Load the settings html, delete the save button
+    const settingsHTML = localStorage.getItem('settingsHTML');
+    if (settingsHTML) {
+        document.getElementById('settingsViewer').innerHTML = settingsHTML;
+
+        const button = document.querySelector('#settingsViewer #saveSettingsBtn');
+        button.remove();
+    }
+    displaySettingsValues("generateFlightF1");
+
+    //Make all the input boxes read only
+    const container = document.getElementById('settingsViewer');
+    const inputs = container.querySelectorAll('input');
+
+    inputs.forEach(input => {
+        input.readOnly = true;
+    });
 });
 
-document.getElementById("testing thing").addEventListener("click", () => {
-    console.log(localStorage.getItem("settingsHTML"));
-    console.log(localStorage.getItem('settingsValues'))
-});
--- electron/gui/assets/js/setting.js
+++ electron/gui/assets/js/setting.js
@@ -7,20 +7,18 @@ const windDir = document.getElementById('windDirection');
 const windVelo = document.getElementById('windVelocity');
 const seedType = document.getElementById('seedSelection')
 const saveSettingsBtn = document.getElementById('saveSettingsBtn')
-//add .value to the end of variable to extract the value of the input
 
 const arrow = document.querySelector('.windIcon .arrow');
 
-// Save the default setting values to local system (will last even if app closes)
-const settingsValues = new Map();
-settingsValues["dispersionDiameter"] = document.getElementById("dispersionDiameter").value;
-settingsValues["dispersionVelocity"] = document.getElementById("dispersionVelocity").value;
-settingsValues["nondispersionVelocity"] = document.getElementById("nondispersionVelocity").value;
-settingsValues["acceleration"] = document.getElementById("acceleration").value;
-settingsValues["minimumCoverage"] = document.getElementById("minimumCoverage").value;
-settingsValues["windDirection"] = document.getElementById("windDirection").value;
-settingsValues["windVelocity"] = document.getElementById("windVelocity").value;
-localStorage.setItem('settingsValues', settingsValues);
+var settingsValues = new Map([
+    ["dispersionDiameter", undefined],
+    ["dispersionVelocity", undefined],
+    ["nondispersionVelocity", undefined],
+    ["acceleration", undefined],
+    ["minimumCoverage", undefined],
+    ["windDirection", undefined],
+    ["windVelocity", undefined]
+]);
 
 function goBack() {
     //TODO: This need to be done without using window.history.back() because we are not redirecting URL.
@@ -33,22 +31,28 @@ function addSeed() {
     console.log("seed Added")
 }
 
+function storeSettingsValues() {
+    for (const key of settingsValues.keys()) {
+        settingsValues.set(key, document.getElementById(key).value);
+    };
+    const settingsValuesArray = Array.from(settingsValues.entries());
+    localStorage.setItem('settingsValues', JSON.stringify(settingsValuesArray));
+};
+
+export function displaySettingsValues(subTabId) {
+    for (const key of settingsValues.keys()) {
+        document.querySelector(`#${subTabId} #${key}`).value = settingsValues.get(key);
+    };
+}
+
 function saveSetting() {
-    //TODO: Need to store the current values somewhere to use.
+    //Save the values in map
+    storeSettingsValues();
+    displaySettingsValues("settingProjectF1");
+
     // Save the setting's HTML structure to local storage
     localStorage.setItem('settingsHTML', document.getElementById('settingProjectF1').innerHTML);
     
-    // Save the changed setting values to local system (will last even if app closes)
-    settingsValues["dispersionDiameter"] = document.getElementById("dispersionDiameter").value;
-    settingsValues["dispersionVelocity"] = document.getElementById("dispersionVelocity").value;
-    settingsValues["nondispersionVelocity"] = document.getElementById("nondispersionVelocity").value;
-    settingsValues["acceleration"] = document.getElementById("acceleration").value;
-    settingsValues["minimumCoverage"] = document.getElementById("minimumCoverage").value;
-    settingsValues["windDirection"] = document.getElementById("windDirection").value;
-    settingsValues["windVelocity"] = document.getElementById("windVelocity").value;
-
-    localStorage.setItem('settingsValues', JSON.stringify(settingsValues));
-
     console.log("Settings saved");
 }
 
@@ -57,6 +61,35 @@ windDir.addEventListener("change", function() {
     var angle = windDir.value
     arrow.style.transform = `rotate(${angle}deg)`;
 }, false)
+
+// Save the default setting values to local system (will last even if app closes), only if not already saved
+if (localStorage.getItem('settingsValues') == null) {
+    storeSettingsValues();
+}
+else {
+    settingsValues = JSON.parse(localStorage.getItem('settingsValues'));
+    settingsValues = new Map(settingsValues);
+    displaySettingsValues("settingProjectF1");
+}
+
+//Observe when we switch to settings subtab, to counteract values being saved without clicking the save button
+const observer = new MutationObserver((mutationsList, observer) => {
+    for (const mutation of mutationsList) {
+        if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+            // Get the updated class attribute value
+            const newClassValue = mutation.target.getAttribute('class');
+            // Check if 'active' is in the new class list
+            if (newClassValue.includes('active')) {
+                displaySettingsValues("settingProjectF1");
+            };
+        };
+    };
+});
+const observerOptions = {
+    attributes: true,
+    attributeFilter: ['class'],
+};
+observer.observe(document.getElementById("settingProjectF1"), observerOptions)
 
 saveSettingsBtn.addEventListener("click", saveSetting);
 
